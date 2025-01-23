fc29122b911d58e5277bd10f1746732fe82d95ef
GerardoLA <gerardolarrinaga@gmail.com>
2025-01-23 13:13:36 +0100

    0_2 tablas de verdad

--- 02_flow_control/02_booleans.py
+++ 02_flow_control/02_booleans.py
@@ -1,30 +1,62 @@
+###
+# 02 - Booleanos
+# Valores lógicos: True (verdadero) y False (falso).
+# Fundamentales para el control de flujo y la lógica en programación.
+###
+
+import os
+os.system("clear")
+
+# Los booleanos representan valoes de verdad;  True o False
+
+print("\n Valores Booleanos básicos:")
+print(True)
+print(False)
+
 # Operadores de comparación : devuelven un valor booleano
-print(5 == 5)  # True
-print(5 == 6)  # False
-print(5 != 6)  # True
-print(5 != 5)  # False
-print(5 > 5)  # False
-print(5 >= 5)  # True
-print(5 < 5)  # False
+print("\n Operadores de comparación")
+print("5 ==5",5 == 5)  # True
+print("5 == 6",5 == 6)  # False
+print("5 != 6",5 != 6)  # True
+print("5 != 5",5 != 5)  # False
+print("5 > 5",5 > 5)  # False
+print("5 >= 5",5 >= 5)  # True
+print("5 < 5",5 < 5)  # False
 
 
 print("\n Comparación de cadenas")
-print("Python" == "Python")  # True
-print("Python" == "python")  # False
-print("Python" != "python")  # True
-print("Python" != "Python")  # False
-print("Python" > "Java")  # True
-print("Python" < "Java")  # False
-print("Python" >= "Java")  # True
-print("Python" <= "Java")  # False
+print("'manzana' < 'pera':", 'manzana' < 'pera') # True
+print("'Hola' == 'hola':", 'Hola' == 'hola') # False
 
+# Operadores lógicos: and, or, not
+print("\n Operadores lógicos")
+print("True and True:",True and True)    # True
+print("True and False:",True and False)  # False
+print("True or False:",True or False)    # True
+print("False or False:",False or False)  # False
+print("not True:",not True)              # False
+print("not False:",not False)            # True
 
 # Tablas de verdad
 print("\n Tablas de verdad")
-print(True and True)  # True
-print(True and False)  # False
-print(False and True)  # False
-print(False and False)  # False
+print("\nand:")
+print("A     B     A and B")
+print("True  True ", True and True)
+print("True  False", True and False)
+print("False True ", False and True)
+print("False False", False and False)
+
+print("\n or:")
+print("A     B     A or B")
+print("True  True ", True or True)
+print("True  False", True or False)
+print("False True ", False or True)
+print("False False", False or False)
+
+print("\n not:") 
+print("A     not A")
+print("True ", not True)
+print("False", not False)
 
 
 print("\La condición ternaria")
