50c323c1521c7ac5943050fda8f3c4d355e42a88
GerardoLA <gerardolarrinaga@gmail.com>
2025-01-24 18:34:01 +0100

    Ejercicio 3 de list_methods

--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -106,3 +106,12 @@ eliminado = lista_a.pop(3)
 print("El elemento eliminado es: ",eliminado)
 lista_b.clear()
 print(lista_b)
+
+# Ejercicio 3: Slicing y eliminación con del
+# Crea una lista con los números del 1 al 10.
+# Utiliza slicing y del para eliminar los elementos desde el índice 2 hasta el 5 (sin incluir el 5).
+# Imprime la lista resultante.
+
+numeros = list(range(1,11))
+del numeros[2:5]
+print(numeros)
