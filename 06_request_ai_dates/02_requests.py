42a66a87f0b03dbc1d87f82aeedc4651b92c1525
gerardoLA <gerardolarrinaga@gmail.com>
2025-04-17 11:56:52 +0200

    completado Midudev ok

--- 06_request_ai_dates/02_requests.py
+++ 06_request_ai_dates/02_requests.py
@@ -20,27 +20,69 @@ except urllib.error.URLError as e:
 
 
 
-# Con dependecia (request)
+# 2. Con dependecia (request)
 import requests
 
 print("\nGET:")
-api_posts = " https://pokeapi.co/api/v2/berry/"
+api_posts = " https://jsonplaceholder.typicode.com/posts"
 response = requests.get(api_posts)
 json_data = response.json()
-print(json_data.keys())
-results = json_data["results"]
-for i in range(0,10):
-    print(f"{i +1} : {results[i]}")
+print(json_data)
 
 # 3. Un POST
 print("\nPOST")
+api_posts ="https://jsonplaceholder.typicode.com/posts/"
+input={
+        "title":"Tateiro probando",
+        "body": "El Real Madrid a semifinales, ah no, que le eliminó el Arsenal",
+        "userId": 1
+}
+response = requests.post(api_posts, json=input)
+print(response.json())
+
+print("\nOtra manera más PRO")
 response = requests.post(
-    "https://jsonplaceholder.typicode.com/posts/",
+    "https://jsonplaceholder.typicode.c/",
     json={
-        "title":"foo",
-        "body": "bar",
+        "title":"Tateiro pro",
+        "body": "Había una vez un circo ",
         "userId": 1
     })
-print(response.status_code)
+print(response.json())
+print(response.status_code) # Estado, 201 sería correcta, si le faltará información debería dar otro.
+
+print("\n Hago un try para comprobar fallo(con direccion inexistente)")
+print("\nOtra manera más PRO")
+try:
+    response = requests.post(
+        "https://jsonplaceholder.typicode.c/",
+        json={
+            "title":"Tateiro pro",
+            "body": "Había una vez un circo ",
+            "userId": 1
+        })
+    print(response.json())
+    print(response.status_code) # Estado, 201 sería correcta, si le faltará información debería dar otro.
+except requests.exceptions.RequestException as e:print(f"No se ha podido hacer, error en la solicitud, revisa la información!: {e}")
+
+print("\nPUT") 
+response = requests.put(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    json={
+        "title":"Tateiro pro",
+        "body": "Había una vez dos circos ",
+        "userId": 1
+    })
+print(response.json())
+
+print("\nPATCH") # A diferencia del PUT aquí no le pasas el objeto entero, sólo pasas la parte que quieres cambiar.
+response = requests.patch(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    json={
+       "title":"En dos palabras im-presionante!"
+    })
+print(response.json())
+
+# Usar la API de GPT-4 o de OPENAI; mirar video de Midudev https://www.twitch.tv/videos/2397854000?filter=archives&sort=time
 
 
--- 06_request_ai_dates/03_clases.py
+++ 06_request_ai_dates/03_clases.py
@@ -15,8 +15,18 @@ class Coche:
         self.modelo = modelo
         self.color = color
         
-    def arrancar(delf):
+    def arrancar(self):
         print(f"El coche {self.marca} {self.modelo} ha arrancado!")
 
 mi_coche = Coche("volskwagen", "Golf", "rojo" )
 mi_coche.arrancar()
+print(mi_coche.tipo)
+
+coche_de_Igor =Coche("Seat", "Ronda", "Rosa")
+coche_de_Igor.arrancar()
+print(coche_de_Igor.modelo)
+
+# Encapsulación: es ocultar los detalles internos de una clase y exponer solo la interfaz pública
+
+# Crear una clase para llamar a la IA  de OpenAI, Deepseek o lo que sea https://www.twitch.tv/videos/2397854000?filter=archives&sort=time
+
